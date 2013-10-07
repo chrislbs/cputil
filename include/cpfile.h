1dc8f26d33700dd3029751a0b50aa910fde03b8b
clbs <chrislbs@gmail.com>
2013-10-07 14:53:15 -0500

    Adding a template header file for file functions.
    Included an initial function for reading all of the lines of a file
    int a back_inserter that takes std::strings as the value type.

---
+++ include/cpfile.h
@@ -0,0 +1,37 @@
+#ifndef _cp_file_h_
+#define _cp_file_h_
+
+#include <fstream>
+#include <string>
+
+namespace cp { namespace util {
+
+template<typename back_inserter>
+void read_lines(
+	const char * filepath, 
+	back_inserter iter)
+{
+	//TODO : ensure back_inserter uses std::string for the value type
+	std::ifstream ifs(filepath, std::ifstream::in);
+
+	char buffer[1024];
+	std::string line;
+	while(!ifs.eof()) 
+	{
+		ifs.getline(buffer, sizeof(buffer));
+		line = buffer;
+		std::cout<< line << std::endl;
+		while(ifs.fail() && !ifs.bad() && !ifs.eof())
+		{
+			ifs.getline(buffer, sizeof(buffer));
+			line += buffer;
+		}
+		
+		*iter = line;
+		++iter;
+	}
+}
+
+} }
+
+#endif
